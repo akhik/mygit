2b056f26c1f6bcd9b8c3ed702ff1a2c188e0d5cd
akhik <akhiksaikia@gmail.com>
2021-10-01 17:09:15 +0530

    this is used to calculate max

---
+++ akhik/loops.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+public class loops {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("enter your first number ");
+        int a =in.nextInt();
+        System.out.println("enter your second number ");
+        int b=in.nextInt();
+        System.out.println("enter your third number ");
+        int c=in.nextInt();
+         int max=a;
+         if(b>max){
+             max=b;}
+
+         if(c>max){
+             max=c;
+         }
+        else {
+             a=max;
+         }
+        System.out.println("the greatest number is" +max);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
